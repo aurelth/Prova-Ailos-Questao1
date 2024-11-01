a8d11ea55beb5100b44b7f44b1feae68390a863f
Aurel Lossou <aurelth@icloud.com>
2024-11-01 15:39:11 -0300

    Classe ContaBancaria criada

---
+++ Questao1/Questao1/ContaBancaria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+{
+    public class ContaBancaria
+    {
+        public int NumeroConta { get; }
+        public string NomeTitular { get; private set; }
+
+        private double Saldo;
+    }
+}
