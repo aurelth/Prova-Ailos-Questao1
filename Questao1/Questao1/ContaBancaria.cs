1d337451729e139bd432adb5cf4f281ed0533813
Aurel Lossou <aurelth@icloud.com>
2024-11-01 15:42:14 -0300

    Criação do método AlterarNomeTitular()

--- Questao1/Questao1/ContaBancaria.cs
+++ Questao1/Questao1/ContaBancaria.cs
@@ -12,5 +12,19 @@ namespace Questao1
         public string NomeTitular { get; private set; }
 
         private double Saldo;
+
+        private const double TaxaSaque = 3.50;
+
+        public ContaBancaria(int numeroConta, string nomeTitular, double depositoInicial = 0.0)
+        {
+            NumeroConta = numeroConta;
+            NomeTitular = nomeTitular;
+            Saldo = depositoInicial;
+        }
+
+        public void AlterarNomeTitular(string novoNome)
+        {
+            NomeTitular = novoNome;
+        }
     }
 }
